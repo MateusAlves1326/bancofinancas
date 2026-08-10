a216953bb0a3b89f5d5b3c1d172ca22a4bced03d
MateusAlves1326 <mateus1326a@gmail.com>
2026-08-10 10:03:25 -0300

    Adiciona estrutura inicial do projeto com Maven, incluindo arquivos de configuração, classes principais e dependências do Spring Boot.

---
+++ src/main/java/acc/br/bancofinancas/BancofinancasApplication.java
@@ -0,0 +1,13 @@
+package acc.br.bancofinancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancofinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancofinancasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/acc/br/bancofinancas/BancofinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package acc.br.bancofinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancofinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
