71ef49996b3d0487dd859ba8d43ade9561caa417
MateusAlves1326 <165835795+MateusAlves1326@users.noreply.github.com>
2026-08-12 11:15:44 -0300

    Refatora AgenciaRepository para usar JpaRepository e implementa AgenciaController para gerenciamento de agências

--- src/main/java/acc/br/bancofinancas/repository/BancoFinancasRepository.java
+++ src/main/java/acc/br/bancofinancas/repository/BancoFinancasRepository.java
@@ -1,10 +1,10 @@
-package acc.br.bancofinancas.repository;
+/*package acc.br.bancofinancas.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
 import acc.br.bancofinancas.model.BancoFinancas;
 public interface BancoFinancasRepository extends CrudRepository<BancoFinancas, Integer> {
-}
+} */
 
 // Avia mais informações sobre o repositório do Banco Finanças, como métodos
 // personalizados para consultas específicas, caso necessário.
