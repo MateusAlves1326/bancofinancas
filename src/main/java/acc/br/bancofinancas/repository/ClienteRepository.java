d053eb5d28b872ce5c18f82cfb9409ad07ea3bad
MateusAlves1326 <165835795+MateusAlves1326@users.noreply.github.com>
2026-08-12 14:35:44 -0300

    Implementa gerenciamento de clientes com DTOs, serviços e repositórios; adiciona validações e controlador para criação de clientes

---
+++ src/main/java/acc/br/bancofinancas/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package acc.br.bancofinancas.repository;
+
+import acc.br.bancofinancas.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+boolean existsByCpf(String cpf);
+  
+}
